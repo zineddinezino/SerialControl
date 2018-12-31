7f5f7821130a0b8b057bd7c7cfb4951ad6b18e5e
zineddinezino <zahouanizineddine@gmail.com>
2018-12-31 16:53:05 +0100

    Expalne

--- HelloWordExample.java
+++ HelloWordExample.java
@@ -1,3 +1,15 @@
+/*
+ git branch Phases
+ git checkout Phases
+ git add -A
+ git commit -m ""
+ git push -u origin Phases
+ git checkout master
+ git pull origin master
+ git merge Phases
+ git push origin master
+*/
+
 public class Example
 {
  public static void main(String [] args)
