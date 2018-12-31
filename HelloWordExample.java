688ac5104ab23b640d59e925be0069e1b1a0a045
zineddinezino <43182889+zineddinezino@users.noreply.github.com>
2018-12-31 18:29:38 +0100

    Updated

--- HelloWordExample.java
+++ HelloWordExample.java
@@ -1,22 +1,10 @@
-/*
- git branch Phases
- git checkout Phases
- git add -A
- git commit -m ""
- git push -u origin Phases
- git checkout master
- git pull origin master
- git merge Phases
- git push origin master
-*/
+
 
 public class Example
 {
  public static void main(String [] args)
  {
-  System.out.println("Hello World");
-  System.out.println("I got this");
-  System.out.println("Yes");    
+  System.out.println("Hello World");  
   return;
  }
 }
